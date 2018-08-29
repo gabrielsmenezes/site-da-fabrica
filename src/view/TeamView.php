9aa96717841c52d80dd1d01f0ac6b88c1c94bd9e
lucasgian <gianextensao@gmail.com>
2018-08-29 09:46:38 -0400

    #1 separando a view de equipes

---
+++ src/view/TeamView.php
@@ -0,0 +1,35 @@
+<?php
+class TeamView extends AdminAbstractView {
+
+	public function index( $args ) { 
+
+		require_once __VIEW__.'team/index.php'; 
+
+	}
+
+	public function edit() { 
+
+		$af = AlunoFactory::get();
+    	$aluno = null;
+
+    	$aluno = $af->getById($_POST['alunoId'])[0]; 
+
+    	require_once __VIEW__.'team/edit.php'; 
+
+	}
+
+
+	public function team() {
+
+		EquipeController::get();
+
+		$af = AlunoFactory::get();
+        
+        $this->index( $af->lista("3") );
+
+	}
+
+	
+
+}
+?>
