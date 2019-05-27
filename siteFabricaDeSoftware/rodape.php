7f9f8dc6ffc93420836f901f962434693f21f28c
Jose <josaugusto37@gmail.com>
2019-05-27 12:44:26 -0400

    novo projeto

--- siteFabricaDeSoftware/rodape.php
+++
@@ -1,2 +0,0 @@
-<p class="direitos">Todos os direitos reservados. Universidade Federal de Mato Grosso do Sul. Copyright&copy;2019</p>
-<p class="endereco">Endereço: Av. Costa e Silva - Vila Olinda, Campo Grande - MS</p>
