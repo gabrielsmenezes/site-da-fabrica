44d664f15e77e1212715a7d6e8a544cf4664f06a
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-23 06:54:58 -0400

    Colocando 3 professores por coluna

--- fabrica/conteudo_professores.php
+++ fabrica/conteudo_professores.php
@@ -1,4 +1,4 @@
-﻿<div id="conteudo_professores_membros">
+﻿<div class="container">
 	<h1 class="tituloProfessor">Professores</h1>
 	<?php
 	include "connect.php";
@@ -19,7 +19,9 @@
 	$contar = mysqli_num_rows($sel_total);
 	$contar_pages = $contar/$qtd_registros;
 	//echo $contar_pages;
-
+	$contador_colunas = 0;
+	echo "<div class='container'>";
+	echo "<div class='row'>";
 	while($line = mysqli_fetch_array($sel_parcial)){
 		$imagem = $line['imagem'];
 		$nome = $line['nome'];
@@ -27,14 +29,23 @@
 		$id_post = $line['id_post'];
 	?>
 
-	<div class="posts_professores">
-		<img src="data:image/jpeg;base64, <?php echo base64_encode($imagem);?>" class="imagem3">
+	<div class="col-4">
+		<img src="data:image/jpeg;base64, <?php echo base64_encode($imagem);?>" width=175 height=auto>
 		<p class="nome3"><?php echo $nome;?></p>
 		<p class="funcao"><?php echo $descricao;?></p>
 	</div>
 
 	<?php
+	$contador_colunas++;
+	if($contador_colunas>2){
+		$contador_colunas=0;
+		echo "</div>";
+		echo "<div class='row'>";
 	}
+}
+	echo "</div>";
+	echo "</div>";
+	
 
 	$anterior = $pagina - 1;
 	$proximo = $pagina + 1;
