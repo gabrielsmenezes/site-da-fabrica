6e6b8a3907701189cf5ca79be465bf844fa2a9d6
Gabriel Menezes <gabrielufmscc@gmail.com>
2019-10-29 21:51:30 -0400

    Arrumando a tela de projeto finalizados e projetos em andamento

--- fabrica/conteudo_emAndamento.php
+++ fabrica/conteudo_emAndamento.php
@@ -1,9 +1,7 @@
-﻿<div id="conteudo_professores_membros">
-	<h1 class="tituloProfessor">Projetos em Andamento</h1>
-	<?php
+﻿	<?php
 	include "connect.php";
 
-	$qtd_registros = 2;
+	$qtd_registros = 10;
 	@$page = $_GET['page'];
 	if(!$page){
 		$pagina = 1;
@@ -19,43 +17,55 @@
 	$contar = mysqli_num_rows($sel_total);
 	$contar_pages = $contar/$qtd_registros;
 	//echo $contar_pages;
-
-	while($line = mysqli_fetch_array($sel_parcial)){
-		$imagem = $line['imagem'];
-		$nome = $line['nome'];
-		$descricao = $line['descricao'];
-		$id_post = $line['id_post'];
-
-
 	?>
+	<div class="container">
+		<div class="container mb-5">
+			<h1 class="tituloProfessor">Projetos em Andamento</h1>
+		</div>
 
-	<div class="posts_professores">
-		<img src="data:image/jpeg;base64, <?php echo base64_encode($imagem);?>" class="imagem3">
-		<p class="nome3"><?php echo $nome;?></p>
-		<p class="funcao"><?php echo $descricao;?></p>
+		<div class="container">
+
+			<?php
+			while($line = mysqli_fetch_array($sel_parcial)){
+				$imagem = $line['imagem'];
+				$nome = $line['nome'];
+				$descricao = $line['descricao'];
+				$id_post = $line['id_post'];
+
+
+			?>
+
+			<div class="row">
+				<div class="col">
+					<img src="data:image/jpeg;base64, <?php echo base64_encode($imagem);?> " width=150px>
+				</div>
+				<div class="col-10">
+					<p class="nome3"><?php echo $nome;?></p>
+					<p class="funcao"><?php echo $descricao;?></p>
+				</div>
+				
+			</div>
+
+			<?php
+			}
+
+			$anterior = $pagina - 1;
+			$proximo = $pagina + 1;
+
+			echo "<br><br>";
+			if($pagina > 1){
+				echo "<a href=?page=$anterior> &larr; Anterior</a>";
+			}
+
+			for($i = 1; $i < $contar_pages + 1; $i++){
+				echo "<a href=?page=".$i.">".$i."</a>";
+			}
+
+
+			if($pagina < $contar_pages){
+				echo "<a href=?page=$proximo>  Próximo &rarr; </a>";
+			}
+
+			?>
+		</div>
 	</div>
-
-	<?php
-	}
-
-	$anterior = $pagina - 1;
-	$proximo = $pagina + 1;
-
-	echo "<br><br>";
-	if($pagina > 1){
-		echo "<a href=?page=$anterior> &larr; Anterior</a>";
-	}
-
-	for($i = 1; $i < $contar_pages + 1; $i++){
-		echo "<a href=?page=".$i.">".$i."</a>";
-	}
-
-
-	if($pagina < $contar_pages){
-		echo "<a href=?page=$proximo>  Próximo &rarr; </a>";
-	}
-
-	?>
-
-	
-</div>
